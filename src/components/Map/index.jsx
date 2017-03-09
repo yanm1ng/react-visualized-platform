38e2123eca62a3e020e27486d71d7891ed54fdf2
yummy1121@foxmail.com <yummy1121@foxmail.com>
2017-03-09 17:27:30 +0800

    add react-router, inner-router

---
+++ src/components/Map/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Map extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+  }
+  render() {
+    return (
+      <div>1212</div>
+    );
+  }
+}
+
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,5 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Login from '../components/Login/index.jsx';
 
-ReactDOM.render(<Login />, document.getElementById('react-content'));
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+import Login from '../components/Login/index.jsx';
+import Index from '../components/Index/index.jsx';
+import Map from '../components/Map/index.jsx';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Router history={hashHistory}>
+          <Route path="/index*" component={Index} />
+          <Route path="/login" component={Login} />
+        </Router>
+        <Router history={hashHistory}>
+          <Route path="/index">
+            <IndexRoute component={Map} />
+            <Route path="/index/map*" component={Map} />
+          </Route>
+        </Router>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('react-content'));
